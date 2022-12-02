67adde51047e043dde10b1c49810c4eea6b96269
Sebastian Stasiak <arionw@live.com>
2022-12-02 03:43:08 +0100

    Move stuff around to make testing easier

--- test/test.hs
+++ test/test.hs
@@ -1,17 +1,25 @@
 import Test.Tasty
 import Test.Tasty.HUnit
+import Text.Printf (printf)
 
+import qualified Day1
 
 main :: IO ()
 main = defaultMain tests
 
-tests :: TestTree
-tests = testGroup "Tests" [ unitTests ]
+getTestFile :: String -> IO String
+getTestFile s =
+    readFile $ printf "test/inputs/%s" s
 
-unitTests :: TestTree
-unitTests = testGroup "UnitTests"
-    [ testCase "Equality" $
-        1 == 1 @?= True
-    , testCase "Inequality" $
-        1 == 2 @?= False
+tests :: TestTree
+tests = testGroup "Tests" [ solutions ]
+
+solutions :: TestTree
+solutions = testGroup "Daily solutions"
+    [ day1 ]
+
+day1 :: TestTree
+day1 = testGroup "Day 1"
+    [ testCase "Part 1" $ Day1.solve1 (getTestFile "Day1") >>= (@?= 24000)
+    , testCase "Part 2" $ Day1.solve2 (getTestFile "Day1") >>= (@?= 45000)
     ]
