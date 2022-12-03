73d3aa27fa46dc4d36e72113283f4bfa80d1bed9
Sebastian Stasiak <12955338+stasiaks@users.noreply.github.com>
2022-12-03 10:13:15 +0100

    Day 3

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 import Common (getInput)
 import qualified Day1
 import qualified Day2
+import qualified Day3
 
 import System.Environment (getArgs)
 import Text.Printf (printf)
@@ -17,6 +18,7 @@ runDay ('D' : 'a' : 'y' : '0' : n) = runDay n
 runDay ('D' : 'a' : 'y' : n) = runDay n
 runDay "1" = printDay (getInput 1) Day1.solve1 Day1.solve2
 runDay "2" = printDay (getInput 2) Day2.solve1 Day2.solve2
+runDay "3" = printDay (getInput 3) Day3.solve1 Day3.solve2
 runDay x = putStrLn $ printf "No solution for day '%s' exists" x
 
 printDay :: Show a => IO String -> (String -> a) -> (String -> a) -> IO ()
