1a4543820827bcd3b1bd9ee31776fb98291ea3df
Sebastian Stasiak <arionw@live.com>
2022-12-06 23:08:43 +0100

    Move Day 1 to parsec

--- lib/Day1.hs
+++ lib/Day1.hs
@@ -1,17 +1,32 @@
 module Day1 (solve1, solve2) where
 
-import Common (getInput, splitOn)
-
 import Data.List
+import Text.ParserCombinators.Parsec
+import Data.Either (fromRight)
 
 solve1 :: String -> Int
-solve1 s = sumHighest 1 $ parse s
+solve1 s = sumHighest 1 $ parseInput s
 
 solve2 :: String -> Int
-solve2 s = sumHighest 3 $ parse s
-
-parse :: String -> [[Int]]
-parse x = (map . map) read $ splitOn "" $ lines x
+solve2 s = sumHighest 3 $ parseInput s
 
 sumHighest :: Int -> [[Int]] -> Int
 sumHighest n xs = sum $ take n $ (reverse . sort) $ map sum xs
+
+-- PARSER --
+
+parseInput :: String -> [[Int]]
+parseInput s = fromRight [] $ parse input [] s
+
+input :: GenParser Char st [[Int]]
+input = sepBy section eol
+
+section :: GenParser Char st [Int]
+section = endBy number eol
+
+number :: GenParser Char st Int
+number = do
+    result <- many1 digit
+    return $ read result
+
+eol = char '\n'
